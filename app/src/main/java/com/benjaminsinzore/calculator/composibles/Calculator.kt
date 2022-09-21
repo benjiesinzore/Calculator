11d81afeaa45de4048f3915e373ca12af1737612
benjiesinzore <benjaminsinzore@gmail.com>
2022-09-21 13:08:07 +0300

    Required Classes and Fields.

---
+++ app/src/main/java/com/benjaminsinzore/calculator/composibles/Calculator.kt
@@ -0,0 +1,14 @@
+package com.benjaminsinzore.calculator.composibles
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.benjaminsinzore.calculator.state.CalculatorState
+
+@Composable
+fun Calculator(
+    state: CalculatorState,
+    modifier: Modifier = Modifier
+) {
+
+
+}
