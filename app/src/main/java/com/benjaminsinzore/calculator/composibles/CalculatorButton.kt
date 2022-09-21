b91eb8cbf59739a5107516bff5fda538868c1ecb
benjiesinzore <benjaminsinzore@gmail.com>
2022-09-21 12:04:39 +0300

    Initial Commit

---
+++ app/src/main/java/com/benjaminsinzore/calculator/composibles/CalculatorButton.kt
@@ -0,0 +1,17 @@
+package com.benjaminsinzore.calculator.composibles
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun CalculatorButton(
+    symbol: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.White,
+    textStyle: TextStyle = TextStyle(),
+    onClick: () -> Unit
+) {
+    
+}
